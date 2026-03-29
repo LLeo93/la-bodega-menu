2bdd4ea6400aef064ffd017a43d4b9810219aeb3
LLeo93 <liba.leoncini@gmail.com>
2026-03-29 15:36:45 +0200

    prove tecniche per prestazioni

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ export default function App() {
 
   return (
     <main className="relative min-h-screen flex flex-col items-center text-white overflow-hidden">
-      <div className="absolute -top-25 w-125 h-125 bg-[#C9A96E]/10 blur-[120px] rounded-full" />
+      {/* <div className="absolute -top-25 w-125 h-125 bg-[#C9A96E]/10 blur-[120px] rounded-full" /> */}
 
       <div className="relative z-10 w-full flex flex-col items-center px-4">
         <Header />
